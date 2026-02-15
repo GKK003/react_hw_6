7cfae11fa21d6567462abecf4b1581e34fe85a5b
Giorgi Kostava <gkostava03@gmail.com>
2026-02-15 17:55:55 +0400

    fixed: oval active

--- src/App.jsx
+++ src/App.jsx
@@ -23,35 +23,35 @@ function App() {
         <TextDiv />
         <div className="oval_nums_div">
           <Oval
-            classname="oval_num"
+            classname={`oval_num ${num1 == "1" ? "oval_num_selected" : ""}`}
             num="1"
             onClick={() => {
               setNum1("1");
             }}
           />
           <Oval
-            classname="oval_num"
+            classname={`oval_num ${num1 === "2" ? "oval_num_selected" : ""}`}
             num="2"
             onClick={() => {
               setNum1("2");
             }}
           />
           <Oval
-            classname="oval_num"
+            classname={`oval_num ${num1 === "3" ? "oval_num_selected" : ""}`}
             num="3"
             onClick={() => {
               setNum1("3");
             }}
           />
           <Oval
-            classname="oval_num"
+            classname={`oval_num ${num1 === "4" ? "oval_num_selected" : ""}`}
             num="4"
             onClick={() => {
               setNum1("4");
             }}
           />
           <Oval
-            classname="oval_num"
+            classname={`oval_num ${num1 === "5" ? "oval_num_selected" : ""}`}
             num="5"
             onClick={() => {
               setNum1("5");
